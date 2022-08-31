e732e64f487f23ced714ccf0b6369adf400ff479
Abhishek S Kurup <abhisatya007@gmail.com>
2022-08-31 16:19:22 +0530

    first commit

---
+++ src/context/GlobalState.js
@@ -0,0 +1,45 @@
+import { type } from "@testing-library/user-event/dist/type";
+import { createContext, useReducer } from "react";
+import AppReducer from "./AppReducer";
+
+//Initial State
+
+const initialState = {
+    transactions: []
+}
+
+//Create Context
+
+export const GlobalContext = createContext(initialState);
+
+//Provider component
+
+export const GlobalProvider = ({children}) => {
+    const [state, dispatch] = useReducer(AppReducer, initialState);
+
+    //Actions
+    function deleteTransaction(id){
+        dispatch({
+            type: 'DELETE_TRANSACTIONS',
+            payload: id
+    });
+
+   }
+
+   function addTransaction(transaction){
+      dispatch({
+        type: 'ADD_TRANSACTIONS',
+        payload: transaction
+      })
+   }
+    
+    return (
+       <GlobalContext.Provider value={{
+        transactions: state.transactions,
+        deleteTransaction,
+        addTransaction
+       }}>
+          {children}
+       </GlobalContext.Provider> 
+    )
+}
